0193d010a8f6bb55c5e0d70bea448057150d6b87
Nicolas Vargas Quiroga <jelboy33@gmail.com>
2019-05-23 16:23:12 -0500

    my first ionic application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NotesPage } from './notes/notes.page';
 
 const routes: Routes = [
   {
@@ -14,6 +15,15 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
+  },
+  {
+    path: 'notes',
+    children: [
+      {
+        path: ':asignature',
+        loadChildren: './notes/notes.module#NotesPageModule'
+      }
+    ] 
   }
 ];
 
--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -9,25 +9,22 @@ export class ListPage implements OnInit {
   private selectedItem: any;
   private icons = [
     'flask',
-    'wifi',
     'beer',
-    'football',
-    'basketball',
     'paper-plane',
     'american-football',
     'boat',
-    'bluetooth',
     'build'
   ];
+  
   public items: Array<{ title: string; note: string; icon: string }> = [];
   constructor() {
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+    const subjects  = ['calculo', 'datawarehouse', 'videojuegos', 'moviles'];
+    const it = subjects.map( subject => ({
+      title: subject,
+      note: "unipahu",
+      icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+    }));
+    this.items = it;
   }
 
   ngOnInit() {
